e1628a2e4a6edf2f7992c135c4d983a24cd88673
Nathaniel Duarte <slacknate@gmail.com>
2015-06-28 02:05:16 -0400

    slackman.args: add ability to be able to add extra arguments to the CLI

--- slackman/args.py
+++ slackman/args.py
@@ -1,8 +1,15 @@
 import argparse
 
 
-def parse_args():
+class Argument(object):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.kwargs = kwargs
+
+
+def parse_args(*arguments):
     parser = argparse.ArgumentParser()
+
     parser.add_argument("--token", dest="token", required=True,
                         help="Authentication token of the slack bot integration that will be connecting.")
     parser.add_argument("--admins", dest="admins", nargs="+", required=True,
@@ -10,4 +17,8 @@ def parse_args():
     parser.add_argument("--log-level", dest="log_level", default="DEBUG", help="Sets the log level.")
     parser.add_argument("--email", dest="email_info", nargs=2, required=True,
                         help="Email address and password the Slack bot uses to send authorization tokens.")
+
+    for argument in arguments:
+        parser.add_argument(argument.name, **argument.kwargs)
+
     return parser.parse_known_args()
